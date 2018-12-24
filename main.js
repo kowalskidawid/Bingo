8ef0ae11d1199abafca455d81e46a02b213566f8
dawid.kowalski <kowalski69dawid@gmail.com>
2018-12-24 10:51:57 +0100

    commit message

--- main.js
+++ main.js
@@ -15,8 +15,8 @@ $(document).ready(function () {
 		koniec_przedzialu += 15;
 	}
 
-	for(let i = 1; i <= 35; i++) {
-		$(karta[i]).on("click", function() {zaznacz(i)});
+	for(let i = 1; i <= 30; i++) {
+		$(karta[i]).on("click", function() {zaznacz(karta[i])});
 	}
 	$("#start_button").click(function() {
 		$("#karta").show();
@@ -28,13 +28,12 @@ $(document).ready(function () {
 });
 
 function zaznacz(a) {
-	console.log(a);
-	var zaznaczona = 1;
+	zaznaczona = $(a).html();
 	var wylosowana = $("#losowanie").html();
 	if(wylosowana == zaznaczona) {
 		trafien++;
-		$("#l" + a).css("background-image", "url(trafienie.png)");
-		$("#l" + a).css("pointer-events", "none");
+		$(a).css("background-image", "url(trafienie.png)");
+		$(a).css("pointer-events", "none");
 		if(trafien == 25) {
 			$("#karta").hide();
 			$("#czas").hide();
@@ -52,5 +51,5 @@ function czas() {
 }
 function losuj() {
 		czas_wylosowania = new Date();
-	$("#losowanie").html(Math.floor(Math.random()*74+1));
+		$("#losowanie").html(Math.floor(Math.random()*74+1));
 }
